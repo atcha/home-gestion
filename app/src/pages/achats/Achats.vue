7d1ccb98cc1e57b55ca224910d5fe5f0b6542bd8
atcha <francois.cornuaud@gmail.com>
2018-01-02 16:45:53 +0100

    UPD - add purchase form creation

--- app/src/pages/achats/Achats.vue
+++ app/src/pages/achats/Achats.vue
@@ -6,7 +6,30 @@
       <q-tab slot="title" name="stats" label="statistiques" icon="pie chart" />
       <q-tab slot="title" name="confs" label="configurations" icon="build" />
 
-      <q-tab-pane name="add">Tab One</q-tab-pane>
+      <q-tab-pane name="add">
+        <div class="form-purchase">
+          <q-input
+            float-label="Nom du produit"
+            v-model="name" />
+          <q-input
+            float-label="Prix du produit"
+            v-model="price" :after="[
+              {
+                icon: 'euro symbol'
+              }
+            ]" />
+          <q-select
+            float-label="Rayons"
+            v-model="shelving"
+            :options="shelvings"
+            multiple="true"
+            radio="true" />
+          <q-select
+            float-label="Magasins"
+            v-model="store"
+            :options="stores" />
+        </div>
+      </q-tab-pane>
       <q-tab-pane name="stats">Tab Three</q-tab-pane>
       <q-tab-pane name="confs">Tab Two</q-tab-pane>
     </q-tabs>
@@ -17,20 +40,50 @@
   import {
     QTabs,
     QTab,
-    QTabPane
+    QTabPane,
+    QInput,
+    QSelect
   } from 'quasar-framework'
   export default {
     name: 'achats',
     components: {
       QTabs,
       QTab,
-      QTabPane
+      QTabPane,
+      QInput,
+      QSelect
     },
     data () {
-      return {}
+      return {
+        name: '',
+        price: '',
+        store: '',
+        shelving: [],
+        stores: [
+          {
+            label: 'Monoprix',
+            value: 'monop'
+          },
+          {
+            label: 'Carrefour',
+            value: 'carrefour'
+          }
+        ],
+        shelvings: [
+          {
+            label: 'Salle de bain',
+            value: 'bathroom'
+          },
+          {
+            label: 'Cuisine',
+            value: 'kitchen'
+          }
+        ]
+      }
     }
   }
 </script>
 
-<style>
+<style lang="stylus">
+  @import 'achats.styl';
 </style>
