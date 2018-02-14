3203af89ad51ac8d28a23a0449a958f425cf22b3
atcha <francois.cornuaud@gmail.com>
2018-02-14 18:12:42 +0100

    UPD - starting style for user account settings

--- app/src/pages/user/Account.vue
+++ app/src/pages/user/Account.vue
@@ -6,9 +6,42 @@
       <q-tab name="modify" slot="title" icon="alarm" label="Modification" />
 
       <q-tab-pane name="resume">
-        <p><img v-bind:src="user.profile_picture" width="40" height="40"></p>
-        <p>{{ user.pseudo }}</p>
-        <p>{{ user.mail }}</p>
+        <div class="row">
+          <div class="col-12 col-md-5">
+            <img v-bind:src="user.profile_picture" class="profilePicture" width="200" height="200">
+          </div>
+          <div class="col-12 col-md-7">
+            <q-field error-label="Le pseudo est obligatoire" class="text-left">
+              <q-input
+                float-label="Pseudo"
+                v-model="user.pseudo"
+              />
+            </q-field>
+            <q-field error-label="Votre e-mail n'est pas valide" class="text-left">
+              <q-input
+                float-label="E-mail"
+                v-model="user.mail"
+              />
+            </q-field>
+            <q-btn color="secondary" @click="" class="full-width" style="margin-top: 15px;">Modifier</q-btn>
+            <q-field error-label="Le mot de passe est obligatoire" class="text-left">
+              <q-input
+                type="password"
+                float-label="Mot de passe"
+                max-length="16"
+                v-model="password"
+              />
+            </q-field>
+            <q-field error-label="Le mot de passe n'est pas identique" class="text-left">
+              <q-input
+                type="password"
+                float-label="Confirmer mot de passe"
+                max-length="16"
+                v-model="confirmpassword"
+              />
+            </q-field>
+          </div>
+        </div>
       </q-tab-pane>
       <q-tab-pane name="modify">Alarms tab</q-tab-pane>
     </q-tabs>
@@ -20,6 +53,8 @@
     QTabs,
     QTab,
     QTabPane,
+    QInput,
+    QBtn,
     SessionStorage
   } from 'quasar-framework'
 
@@ -28,11 +63,17 @@
     components: {
       QTabs,
       QTab,
-      QTabPane
+      QTabPane,
+      QBtn,
+      QInput
     },
     data () {
       return {
-        user: {}
+        user: {
+          pseudo: '',
+          mail: ''
+        },
+        url: ''
       }
     },
     mounted () {
