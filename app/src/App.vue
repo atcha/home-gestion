19f882b12035eae3137ac498767a5d415eb34e70
atcha <francois.cornuaud@gmail.com>
2018-02-15 10:36:43 +0100

    UPD - Header styling

--- app/src/App.vue
+++ app/src/App.vue
@@ -5,45 +5,56 @@
     <main>
       <q-layout ref="layout" view="hHh Lpr fff" :left-class="{'bg-primary': true}">
         <!-- Header -->
-        <q-toolbar slot="header">
-          <q-btn v-if="authenticated" flat @click="$refs.layout.toggleLeft()">
-            <q-icon name="menu" />
+        <q-toolbar slot="header" class="items-stretch">
+          <q-btn
+            v-if="authenticated"
+            flat
+            @click="menuExpand()"
+            class="burger-icon self-center"
+            :icon="openedMenu ? 'keyboard arrow left' : 'menu'"
+          >
           </q-btn>
-          <div class="logo-container">
+          <div class="logo-container self-center">
             <object type="image/svg+xml" data="~assets/logo-home-gestion.svg" width="30" height="30">
               <img src="~assets/logo-home-gestion.png" width="30" height="30" />
             </object>
           </div>
-          <q-toolbar-title>
+          <q-toolbar-title class="self-center">
             Home gestion
             <span slot="subtitle">Outils d'administration des tâches et dépenses</span>
           </q-toolbar-title>
-          <q-btn v-if="authenticated" flat>
-            <q-icon name="account_box" />
-            <q-popover ref="popover" class="user-popover">
-              <q-list link>
-                <q-item to="/account/user-purchases">
-                  <q-item-side left icon="fa-shopping-bag" />
-                  <q-item-main label="Mes achats" />
-                </q-item>
-                <q-item to="/account/user-stats">
-                  <q-item-side left icon="pie chart" />
-                  <q-item-main label="Statistiques" />
-                </q-item>
-                <q-item to="/account/user-settings">
-                  <q-item-side left icon="fa-user" />
-                  <q-item-main label="Mon compte" />
-                </q-item>
-                <q-item @click="logout">
-                  <q-item-side left icon="fa-sign-out" />
-                  <q-item-main label="Déconnexion" />
-                </q-item>
-              </q-list>
-            </q-popover>
-          </q-btn>
-          <q-btn @click="$router.push('/help')" flat>
-            <q-icon name="help" />
-          </q-btn>
+          <ul class="top-menu-right">
+            <li v-if="authenticated">
+              <q-btn flat>
+                <q-icon name="account_box" />
+                <q-popover ref="popover" class="user-popover">
+                  <q-list link>
+                    <q-item to="/account/user-purchases">
+                      <q-item-side left icon="fa-shopping-bag" />
+                      <q-item-main label="Mes achats" />
+                    </q-item>
+                    <q-item to="/account/user-stats">
+                      <q-item-side left icon="pie chart" />
+                      <q-item-main label="Statistiques" />
+                    </q-item>
+                    <q-item to="/account/user-settings">
+                      <q-item-side left icon="fa-user" />
+                      <q-item-main label="Mon compte" />
+                    </q-item>
+                    <q-item @click="logout">
+                      <q-item-side left icon="fa-sign-out" />
+                      <q-item-main label="Déconnexion" />
+                    </q-item>
+                  </q-list>
+                </q-popover>
+              </q-btn>
+            </li>
+            <li>
+              <q-btn @click="$router.push('/help')" flat>
+                <q-icon name="help" />
+              </q-btn>
+            </li>
+          </ul>
         </q-toolbar>
         <sidebar v-if="authenticated" slot="left" />
         <!-- sub-routes get injected here: -->
@@ -96,16 +107,28 @@
     data () {
       return {
         authenticated: false,
-        user: {}
+        user: {},
+        openedMenu: true
       }
     },
     methods: {
       logout: function () {
+        // Signout with firebase
         firebase.auth().signOut()
           .then(() => {
-            SessionStorage.set('authenticate', false)
+            SessionStorage.clear()
             this.$router.replace('/login')
           })
+      },
+      menuExpand: function () {
+        // Used to change icon when status menu changed
+        this.$refs.layout.toggleLeft()
+        if (!this.openedMenu) {
+          this.openedMenu = true
+        }
+        else {
+          this.openedMenu = false
+        }
       }
     },
     mounted () {
